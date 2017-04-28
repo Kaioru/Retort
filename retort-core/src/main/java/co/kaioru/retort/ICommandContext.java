69c50c6cb607334402d19f7eb969e9263cd859c3
Kaioru <keifuurawrs@gmail.com>
2017-04-28 18:48:35 +0800

    Tweak all objects to be serializable

--- retort-core/src/main/java/co/kaioru/retort/ICommandContext.java
+++ retort-core/src/main/java/co/kaioru/retort/ICommandContext.java
@@ -1,8 +1,9 @@
 package co.kaioru.retort;
 
+import java.io.Serializable;
 import java.util.Queue;
 
-public interface ICommandContext {
+public interface ICommandContext extends Serializable {
 
     Queue<String> getArgs();
 
--- retort-core/src/main/java/co/kaioru/retort/ICommandExecutable.java
+++ retort-core/src/main/java/co/kaioru/retort/ICommandExecutable.java
@@ -2,7 +2,9 @@ package co.kaioru.retort;
 
 import co.kaioru.retort.exception.CommandException;
 
-public interface ICommandExecutable<I extends ICommandContext, O> {
+import java.io.Serializable;
+
+public interface ICommandExecutable<I extends ICommandContext, O> extends Serializable {
 
     O execute(I i) throws CommandException;
 
