0ff0ff90d41637f93e2a590d8d96e4bec3756ad9
Kaioru <keifuurawrs@gmail.com>
2017-04-29 10:59:16 +0800

    Add reference annotation and generators

---
+++ retort-annotations/src/main/lombok/co/kaioru/retort/annotation/adapter/ReferenceAnnotationGenerator.java
@@ -0,0 +1,28 @@
+package co.kaioru.retort.annotation.adapter;
+
+import co.kaioru.retort.ICommand;
+import co.kaioru.retort.ICommandContext;
+import co.kaioru.retort.annotation.AbstractAnnotationGenerator;
+import co.kaioru.retort.annotation.Reference;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReferenceAnnotationGenerator<I extends ICommandContext, O> extends AbstractAnnotationGenerator<I, O> {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public ICommand<I, O> generate(Object object, Method method) {
+        ICommand<I, O> command = null;
+
+        if (method.isAnnotationPresent(Reference.class)) {
+            try {
+                command = (ICommand<I, O>) method.invoke(object);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                return null;
+            }
+        }
+        return command;
+    }
+
+}
