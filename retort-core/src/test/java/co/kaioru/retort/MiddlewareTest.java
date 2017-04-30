65ecf54b31c745358af00b911d1cf327f26669a6
Kaioru <keifuurawrs@gmail.com>
2017-04-30 09:33:11 +0800

    Add middleware tests

---
+++ retort-core/src/test/java/co/kaioru/retort/MiddlewareTest.java
@@ -0,0 +1,58 @@
+package co.kaioru.retort;
+
+import co.kaioru.retort.builder.CommandBuilder;
+import co.kaioru.retort.builder.MiddlewareBuilder;
+import co.kaioru.retort.exception.CommandException;
+import co.kaioru.retort.exception.CommandMiddlewareException;
+import co.kaioru.retort.exception.CommandNotBuiltException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertTrue;
+
+public class MiddlewareTest {
+
+    @Rule
+    public ExpectedException exceptions = ExpectedException.none();
+
+    private ICommandRegistry<ICommandContext, Boolean> commandRegistry;
+    private ICommand<ICommandContext, Boolean> command;
+
+    @Before
+    public void setup() {
+        this.commandRegistry = new CommandRegistry<>("registry");
+        this.command = new CommandBuilder<ICommandContext, Boolean>("true")
+                .build(ctx -> true);
+    }
+
+    @Test
+    public void middlewarePass() throws CommandException {
+        command.registerMiddleware(new MiddlewareBuilder<>()
+                .build(context -> true));
+        commandRegistry.registerCommand(command);
+
+        assertTrue(commandRegistry.execute(new CommandContext(), "true"));
+    }
+
+    @Test
+    public void middlewareFail() throws CommandException {
+        command.registerMiddleware(new MiddlewareBuilder<>()
+                .build(context -> false));
+        commandRegistry.registerCommand(command);
+
+        exceptions.expect(CommandMiddlewareException.class);
+        commandRegistry.execute(new CommandContext(), "true");
+    }
+
+    @Test
+    public void middlewareNotBuiltException() throws CommandException {
+        command.registerMiddleware(new MiddlewareBuilder<>());
+        commandRegistry.registerCommand(command);
+
+        exceptions.expect(CommandMiddlewareException.class);
+        commandRegistry.execute(new CommandContext(), "true");
+    }
+
+}
