903b30c496a5ca4ed8e96bdb83274941d18ca2c5
Kaioru <keifuurawrs@gmail.com>
2017-04-30 17:19:37 +0800

    Move logic to abstract class

--- retort-core/src/main/java/co/kaioru/retort/ICommand.java
+++ retort-core/src/main/java/co/kaioru/retort/ICommand.java
@@ -1,11 +1,10 @@
 package co.kaioru.retort;
 
 import co.kaioru.retort.exception.CommandException;
-import co.kaioru.retort.exception.CommandMiddlewareException;
-import co.kaioru.retort.exception.CommandNotFoundException;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 public interface ICommand<I extends ICommandContext, O> extends ICommandExecutable<I, O> {
 
@@ -57,43 +56,8 @@ public interface ICommand<I extends ICommandContext, O> extends ICommandExecutab
         getCommands().remove(command);
     }
 
-    default List<ICommand<I, O>> getCommand(String name) {
-        return getCommands().stream()
-                .filter(cmd -> cmd.getName().toLowerCase().startsWith(name)
-                        || cmd.getAliases().stream().anyMatch(s -> s.toLowerCase().startsWith(name)))
-                .collect(Collectors.toList());
-    }
+    List<ICommand<I, O>> getCommand(String name);
 
-    default O process(I i) throws CommandException {
-        LinkedList<String> args = i.getArgs();
-
-        if (args.size() > 0) {
-            String first = args.peek();
-            Optional<ICommand<I, O>> opt = getCommand(first)
-                    .stream()
-                    .findFirst();
-            if (opt.isPresent()) {
-                args.remove();
-                return (O) opt.get().process(i);
-            }
-        }
-
-        if (this instanceof ICommandRegistry)
-            throw new CommandNotFoundException();
-
-        if (getMiddlewares().stream()
-                .map(m -> {
-                    try {
-                        return m.execute(i);
-                    } catch (CommandException e) {
-                        return false;
-                    }
-                })
-                .filter(b -> !b)
-                .count() > 0)
-            throw new CommandMiddlewareException();
-
-        return execute(i);
-    }
+    O process(I i) throws CommandException;
 
 }
--- retort-core/src/main/lombok/co/kaioru/retort/AbstractCommand.java
+++ retort-core/src/main/lombok/co/kaioru/retort/AbstractCommand.java
@@ -1,10 +1,13 @@
 package co.kaioru.retort;
 
+import co.kaioru.retort.exception.CommandException;
+import co.kaioru.retort.exception.CommandMiddlewareException;
+import co.kaioru.retort.exception.CommandNotFoundException;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -21,6 +24,47 @@ public abstract class AbstractCommand<I extends ICommandContext, O> implements I
         this.aliases = new HashSet<>();
         this.middlewares = new HashSet<>();
         this.commands = new HashSet<>();
+    }
+
+    @Override
+    public List<ICommand<I, O>> getCommand(String name) {
+        return getCommands().stream()
+                .filter(cmd -> cmd.getName().toLowerCase().startsWith(name)
+                        || cmd.getAliases().stream().anyMatch(s -> s.toLowerCase().startsWith(name)))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public O process(I i) throws CommandException {
+        LinkedList<String> args = i.getArgs();
+
+        if (args.size() > 0) {
+            String first = args.peek();
+            Optional<ICommand<I, O>> opt = getCommand(first)
+                    .stream()
+                    .findFirst();
+            if (opt.isPresent()) {
+                args.remove();
+                return (O) opt.get().process(i);
+            }
+        }
+
+        if (this instanceof ICommandRegistry)
+            throw new CommandNotFoundException();
+
+        if (getMiddlewares().stream()
+                .map(m -> {
+                    try {
+                        return m.execute(i);
+                    } catch (CommandException e) {
+                        return false;
+                    }
+                })
+                .filter(b -> !b)
+                .count() > 0)
+            throw new CommandMiddlewareException();
+
+        return execute(i);
     }
 
 }
