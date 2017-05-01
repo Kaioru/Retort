110c83caed2355682aa0213e760f3f5df55d95da
Kaioru <keifuurawrs@gmail.com>
2017-05-01 10:00:41 +0800

    Add basic annotation processors

---
+++ retort-annotations/src/main/kotlin/co/kaioru/retort/annotation/impl/adapter/AliasAnnotationProcessor.kt
@@ -0,0 +1,14 @@
+package co.kaioru.retort.annotation.impl.adapter
+
+import co.kaioru.retort.ICommand
+import co.kaioru.retort.annotation.impl.AnnotationProcessor
+import co.kaioru.retort.annotation.type.Alias
+import java.lang.reflect.Method
+
+class AliasAnnotationProcessor<I, O> : AnnotationProcessor<I, O>() {
+    override fun process(command: ICommand<I, O>, any: Any, method: Method) {
+        method.getAnnotationsByType(Alias::class.java).forEach {
+            command.registerAlias(it.value)
+        }
+    }
+}
---
+++ retort-annotations/src/main/kotlin/co/kaioru/retort/annotation/impl/adapter/DescriptionAnnotationProcessor.kt
@@ -0,0 +1,13 @@
+package co.kaioru.retort.annotation.impl.adapter
+
+import co.kaioru.retort.ICommand
+import co.kaioru.retort.annotation.impl.AnnotationProcessor
+import co.kaioru.retort.annotation.type.Description
+import java.lang.reflect.Method
+
+class DescriptionAnnotationProcessor<I, O> : AnnotationProcessor<I, O>() {
+    override fun process(command: ICommand<I, O>, any: Any, method: Method) {
+        if (method.isAnnotationPresent(Description::class.java))
+            command.description = method.getAnnotation(Description::class.java).value
+    }
+}
